d49631c563eb7ef396a7d3027b106f4aa137c845
MrBeanWach <qdhthien20@clc.fitus.edu.vn>
2022-11-29 12:45:56 +0700

    update

--- controllers/task2Controller.js
+++ controllers/task2Controller.js
@@ -6,7 +6,7 @@ controller.show = (req,res) => {
     let jar55 = (salary * 0.55).toFixed(2);
     let jar5 = (salary * 0.05).toFixed(2);
     let jar10= (salary * 0.1).toFixed(2);
-
+    res.locals.Ma = '20127041 - Quách Đình Huy Thiện  ';
     res.render('task2',{title :Title, jar55, jar5, jar10});
 }
 controller.showPost = (req,res)=>{
@@ -14,7 +14,7 @@ controller.showPost = (req,res)=>{
     let jar55 = (salary * 0.55).toFixed(2);
     let jar5 = (salary * 0.05).toFixed(2);
     let jar10= (salary * 0.1).toFixed(2);
-
+    res.locals.Ma = '20127041 - Quách Đình Huy Thiện  ';
     res.render('task2',{title :Title, jar55, jar5, jar10});
 }
 
